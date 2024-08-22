6892f9b889fdbb87e096a24a13a2bccf3b82afa9
PopeyeZhong <9555843@qq.com>
2024-08-22 17:01:06 +0800

    调整MySQL字符集。 :chicken:

--- database/Administratives-China-Tables(mysql).sql
+++ database/Administratives-China-Tables(mysql).sql
@@ -3,9 +3,9 @@ SET TIME_ZONE='+08:00';
 
 CREATE TABLE IF NOT EXISTS `Province` (
   `ProvinceId` tinyint unsigned NOT NULL COMMENT '主键，省份代码',
-  `Name`       varchar(50)      NOT NULL COMMENT '省份名称' COLLATE 'utf8mb4_general_ci',
-  `Abbr`       varchar(50)      NOT NULL COMMENT '省份简称' COLLATE 'utf8mb4_general_ci',
-  `Alias`      char(1)          NULL     COMMENT '省份代号' COLLATE 'utf8mb4_general_ci',
+  `Name`       varchar(50)      NOT NULL COMMENT '省份名称' COLLATE 'utf8mb4_0900_ai_ci',
+  `Abbr`       varchar(50)      NOT NULL COMMENT '省份简称' COLLATE 'utf8mb4_0900_ai_ci',
+  `Alias`      char(1)          NULL     COMMENT '省份代号' COLLATE 'utf8mb4_0900_ai_ci',
   `Acronym`    varchar(50)      NULL     COMMENT '名称缩写' COLLATE 'ascii_general_ci',
   PRIMARY KEY (`ProvinceId`)
 ) ENGINE=InnoDB COMMENT='省份表';
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS `Province` (
 CREATE TABLE IF NOT EXISTS `City` (
   `ProvinceId` tinyint unsigned NOT NULL COMMENT '主键，省份代码',
   `CityId`     tinyint unsigned NOT NULL COMMENT '主键，城市代码',
-  `Name`       varchar(50)      NOT NULL COMMENT '城市名称' COLLATE 'utf8mb4_general_ci',
+  `Name`       varchar(50)      NOT NULL COMMENT '城市名称' COLLATE 'utf8mb4_0900_ai_ci',
   `Acronym`    varchar(50)      NULL     COMMENT '名称缩写' COLLATE 'ascii_general_ci',
   PRIMARY KEY (`ProvinceId`,`CityId`)
 ) ENGINE=InnoDB COMMENT='城市表';
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS `District` (
   `ProvinceId` tinyint unsigned NOT NULL COMMENT '主键，省份代码',
   `CityId`     tinyint unsigned NOT NULL COMMENT '主键，城市代码',
   `DistrictId` tinyint unsigned NOT NULL COMMENT '主键，区县代码',
-  `Name`       varchar(50)      NOT NULL COMMENT '区县名称' COLLATE 'utf8mb4_general_ci',
+  `Name`       varchar(50)      NOT NULL COMMENT '区县名称' COLLATE 'utf8mb4_0900_ai_ci',
   `Acronym`    varchar(50)      NULL     COMMENT '名称缩写' COLLATE 'ascii_general_ci',
   `PostalCode` char(6)          NULL     COMMENT '邮政编码' COLLATE 'ascii_general_ci',
   PRIMARY KEY (`ProvinceId`,`CityId`,`DistrictId`)
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS `Street` (
   `CityId`     tinyint unsigned NOT NULL COMMENT '主键，城市代码',
   `DistrictId` tinyint unsigned NOT NULL COMMENT '主键，区县代码',
   `StreetId`   tinyint unsigned NOT NULL COMMENT '主键，街道代码',
-  `Name`       varchar(50)      NOT NULL COMMENT '街道名称' COLLATE 'utf8mb4_general_ci',
+  `Name`       varchar(50)      NOT NULL COMMENT '街道名称' COLLATE 'utf8mb4_0900_ai_ci',
   `Acronym`    varchar(50)      NULL     COMMENT '名称缩写' COLLATE 'ascii_general_ci',
   `PostalCode` char(6)          NULL     COMMENT '邮政编码' COLLATE 'ascii_general_ci',
   PRIMARY KEY (`ProvinceId`,`CityId`,`DistrictId`,`StreetId`)
