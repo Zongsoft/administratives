6558cd52e7e33636ee28cfe4ccb2f6a6b24052b0
Popeye Zhong <zongsoft@qq.com>
2026-02-18 00:35:46 +0800

    调整文档格式。 :fu:

--- database/Administratives-China-Tables(postgres).sql
+++ database/Administratives-China-Tables(postgres).sql
@@ -1,17 +1,17 @@
 CREATE TABLE IF NOT EXISTS "Province"
 (
-    "ProvinceId" smallint    NOT NULL,
-    "Name"       varchar(50) NOT NULL COLLATE "C.utf8",
-    "Abbr"       varchar(50) NOT NULL COLLATE "C.utf8",
-    "Alias"      char(1)     COLLATE "C.utf8",
-    "Acronym"    varchar(50) COLLATE "C",
-    PRIMARY KEY ("ProvinceId")
+	"ProvinceId" smallint    NOT NULL,
+	"Name"       varchar(50) NOT NULL COLLATE "C.utf8",
+	"Abbr"       varchar(50) NOT NULL COLLATE "C.utf8",
+	"Alias"      char(1)     COLLATE "C.utf8",
+	"Acronym"    varchar(50) COLLATE "C",
+	PRIMARY KEY ("ProvinceId")
 );
 
 CREATE INDEX IF NOT EXISTS "IX_Province_Name" ON "Province" USING btree
-  ("Name" ASC);
+	("Name" ASC);
 CREATE INDEX IF NOT EXISTS "IX_Province_Acronym" ON "Province" USING btree
-  ("Acronym" ASC);
+	("Acronym" ASC);
 
 COMMENT ON TABLE "Province" IS '省份表';
 COMMENT ON COLUMN "Province"."ProvinceId" IS '主键，省份代码';
@@ -21,17 +21,17 @@ COMMENT ON COLUMN "Province"."Alias"      IS '省份代号';
 COMMENT ON COLUMN "Province"."Acronym"    IS '名称缩写';
 
 CREATE TABLE IF NOT EXISTS "City" (
-  "ProvinceId" smallint         NOT NULL,
-  "CityId"     smallint         NOT NULL,
-  "Name"       varchar(50)      NOT NULL COLLATE "C.utf8",
-  "Acronym"    varchar(50)      NULL     COLLATE "C",
-  PRIMARY KEY ("ProvinceId", "CityId")
+	"ProvinceId" smallint    NOT NULL,
+	"CityId"     smallint    NOT NULL,
+	"Name"       varchar(50) NOT NULL COLLATE "C.utf8",
+	"Acronym"    varchar(50) NULL     COLLATE "C",
+	PRIMARY KEY ("ProvinceId", "CityId")
 );
 
 CREATE INDEX IF NOT EXISTS "IX_City_Name" ON "City" USING btree
-  ("Name" ASC);
+	("Name" ASC);
 CREATE INDEX IF NOT EXISTS "IX_City_Acronym" ON "City" USING btree
-  ("Acronym" ASC);
+	("Acronym" ASC);
 
 COMMENT ON TABLE "City" IS '城市表';
 COMMENT ON COLUMN "City"."ProvinceId" IS '主键，省份代码';
@@ -40,21 +40,21 @@ COMMENT ON COLUMN "City"."Name"       IS '城市名称';
 COMMENT ON COLUMN "City"."Acronym"    IS '名称缩写';
 
 CREATE TABLE IF NOT EXISTS "District" (
-  "ProvinceId" smallint         NOT NULL,
-  "CityId"     smallint         NOT NULL,
-  "DistrictId" smallint         NOT NULL,
-  "Name"       varchar(50)      NOT NULL COLLATE "C.utf8",
-  "Acronym"    varchar(50)      NULL     COLLATE "C",
-  "PostalCode" char(6)          NULL     COLLATE "C",
-  PRIMARY KEY ("ProvinceId", "CityId", "DistrictId")
+	"ProvinceId" smallint    NOT NULL,
+	"CityId"     smallint    NOT NULL,
+	"DistrictId" smallint    NOT NULL,
+	"Name"       varchar(50) NOT NULL COLLATE "C.utf8",
+	"Acronym"    varchar(50) NULL     COLLATE "C",
+	"PostalCode" char(6)     NULL     COLLATE "C",
+	PRIMARY KEY ("ProvinceId", "CityId", "DistrictId")
 );
 
 CREATE INDEX IF NOT EXISTS "IX_District_Name" ON "District" USING btree
-  ("Name" ASC);
+	("Name" ASC);
 CREATE INDEX IF NOT EXISTS "IX_District_Acronym" ON "District" USING btree
-  ("Acronym" ASC);
+	("Acronym" ASC);
 CREATE INDEX IF NOT EXISTS "IX_District_PostalCode" ON "District" USING btree
-  ("PostalCode" ASC);
+	("PostalCode" ASC);
 
 COMMENT ON TABLE "District" IS '区县表';
 COMMENT ON COLUMN "District"."ProvinceId" IS '主键，省份代码';
@@ -65,22 +65,22 @@ COMMENT ON COLUMN "District"."Acronym"    IS '名称缩写';
 COMMENT ON COLUMN "District"."PostalCode" IS '邮政编码';
 
 CREATE TABLE IF NOT EXISTS "Street" (
-  "ProvinceId" smallint         NOT NULL,
-  "CityId"     smallint         NOT NULL,
-  "DistrictId" smallint         NOT NULL,
-  "StreetId"   smallint         NOT NULL,
-  "Name"       varchar(50)      NOT NULL COLLATE "C.utf8",
-  "Acronym"    varchar(50)      NULL     COLLATE "C",
-  "PostalCode" char(6)          NULL     COLLATE "C",
-  PRIMARY KEY ("ProvinceId", "CityId", "DistrictId", "StreetId")
+	"ProvinceId" smallint    NOT NULL,
+	"CityId"     smallint    NOT NULL,
+	"DistrictId" smallint    NOT NULL,
+	"StreetId"   smallint    NOT NULL,
+	"Name"       varchar(50) NOT NULL COLLATE "C.utf8",
+	"Acronym"    varchar(50) NULL     COLLATE "C",
+	"PostalCode" char(6)     NULL     COLLATE "C",
+	PRIMARY KEY ("ProvinceId", "CityId", "DistrictId", "StreetId")
 );
 
 CREATE INDEX IF NOT EXISTS "IX_Street_Name" ON "Street" USING btree
-  ("Name" ASC);
+	("Name" ASC);
 CREATE INDEX IF NOT EXISTS "IX_Street_Acronym" ON "Street" USING btree
-  ("Acronym" ASC);
+	("Acronym" ASC);
 CREATE INDEX IF NOT EXISTS "IX_Street_PostalCode" ON "Street" USING btree
-  ("PostalCode" ASC);
+	("PostalCode" ASC);
 
 COMMENT ON TABLE "Street" IS '街道(乡镇)表';
 COMMENT ON COLUMN "Street"."ProvinceId" IS '主键，省份代码';
--- database/Administratives-China-Tables.md
+++ database/Administratives-China-Tables.md
@@ -4,43 +4,43 @@
 
 字段名称 | 数据类型 | 长度 | 可空 | 备注
 ------- |:-------:|:---:|:---:| ----
-ProvinceId  | byte      | 1     | ✗ | 主键，省份代码
-Name        | nvarchar  | 50    | ✗ | 省份名称
-Abbr        | nvarchar  | 50    | ✗ | 省份简称
-Alias       | nchar     | 1     | ✓ | 省份代名
-Acronym     | varchar   | 50    | ✓ | 名称缩写
+ProvinceId | byte     | 1  | ✗ | 主键，省份代码
+Name       | nvarchar | 50 | ✗ | 省份名称
+Abbr       | nvarchar | 50 | ✗ | 省份简称
+Alias      | nchar    | 1  | ✓ | 省份代名
+Acronym    | varchar  | 50 | ✓ | 名称缩写
 
 
 ## 城市表 `City`
 
 字段名称 | 数据类型 | 长度 | 可空 | 备注
 ------- |:-------:|:---:|:---:| ----
-ProvinceId  | byte      | 1     | ✗ | 主键，省份代码
-CityId      | byte      | 1     | ✗ | 主键，城市代码
-Name        | nvarchar  | 50    | ✗ | 城市名称
-Acronym     | varchar   | 50    | ✓ | 名称缩写
+ProvinceId | byte     | 1  | ✗ | 主键，省份代码
+CityId     | byte     | 1  | ✗ | 主键，城市代码
+Name       | nvarchar | 50 | ✗ | 城市名称
+Acronym    | varchar  | 50 | ✓ | 名称缩写
 
 
 ## 区县表 `District`
 
 字段名称 | 数据类型 | 长度 | 可空 | 备注
 ------- |:-------:|:---:|:---:| ----
-ProvinceId  | byte      | 1     | ✗ | 主键，省份代码
-CityId      | byte      | 1     | ✗ | 主键，城市代码
-DistrictId  | byte      | 1     | ✗ | 主键，区县代码
-Name        | nvarchar  | 50    | ✗ | 区县名称
-Acronym     | varchar   | 50    | ✓ | 名称缩写
-PostalCode  | char      | 6     | ✓ | 邮政编码
+ProvinceId | byte     | 1  | ✗ | 主键，省份代码
+CityId     | byte     | 1  | ✗ | 主键，城市代码
+DistrictId | byte     | 1  | ✗ | 主键，区县代码
+Name       | nvarchar | 50 | ✗ | 区县名称
+Acronym    | varchar  | 50 | ✓ | 名称缩写
+PostalCode | char     | 6  | ✓ | 邮政编码
 
 
 ## 街道(乡镇)表 `Street`
 
 字段名称 | 数据类型 | 长度 | 可空 | 备注
 ------- |:-------:|:---:|:---:| ----
-ProvinceId  | byte      | 1     | ✗ | 主键，省份代码
-CityId      | byte      | 1     | ✗ | 主键，城市代码
-DistrictId  | byte      | 1     | ✗ | 主键，区县代码
-StreetId    | byte      | 1     | ✗ | 主键，街道代码
-Name        | nvarchar  | 50    | ✗ | 街道名称
-Acronym     | varchar   | 50    | ✓ | 名称缩写
-PostalCode  | char      | 6     | ✓ | 邮政编码
+ProvinceId | byte     | 1  | ✗ | 主键，省份代码
+CityId     | byte     | 1  | ✗ | 主键，城市代码
+DistrictId | byte     | 1  | ✗ | 主键，区县代码
+StreetId   | byte     | 1  | ✗ | 主键，街道代码
+Name       | nvarchar | 50 | ✗ | 街道名称
+Acronym    | varchar  | 50 | ✓ | 名称缩写
+PostalCode | char     | 6  | ✓ | 邮政编码
